8a992f03948b959beedc397bd3c004e780196766
xiaoshuyui <528490652@qq.com>
2022-08-13 14:59:27 +0800

    [mltools_viewer] chore: add some codes

--- mltools_viewer/lib/screens/nn_graph/components/nn_flow_graph_view.dart
+++ mltools_viewer/lib/screens/nn_graph/components/nn_flow_graph_view.dart
@@ -9,34 +9,75 @@ import '../model/nn_nodes.dart';
 import 'nn_node_widget.dart';
 
 /// this is a modification of [flow_graph_view] to draw nn graph
-
-class NNFlowGraphView<T extends NNNode> extends StatelessWidget {
-  NNFlowGraphView(
+class NNFlowGraphView<T extends NNNode> extends StatefulWidget {
+  const NNFlowGraphView(
       {Key? key,
-      required this.root,
+      this.root,
       this.direction = Axis.horizontal,
       this.onSelectChanged,
       this.onEdgeColor,
       this.messageStyle,
-      required this.nodeMessageMap})
-      : super(key: key);
-  final GraphNode<T> root;
+      required this.nodeMessageMap,
+      this.structure})
+      : assert(root != null || structure != null),
+        super(key: key);
+  final GraphNode<T>? root;
   final Axis direction;
   final OnSelectChanged<T>? onSelectChanged;
   final OnEdgeColor<T>? onEdgeColor;
   final TextStyle? messageStyle;
   final Map<int, String> nodeMessageMap;
+  final Map<String, dynamic>? structure;
+
+  @override
+  State<NNFlowGraphView> createState() => _NNFlowGraphViewState();
+}
+
+class _NNFlowGraphViewState<T extends NNNode>
+    extends State<NNFlowGraphView<T>> {
+  late Graph<T> graph;
   final GraphFocusManager _focusManager = GraphFocusManager();
 
-  late Graph<T> graph;
-
   Map<String, dynamic> _dumpToJson() {
+    Map<String, dynamic> result = {"items": []};
     final nodes = graph.nodes as List<GraphNode<T>>;
+    for (final i in nodes) {
+      result["items"].add(i.data!.toJson());
+    }
 
-    return {};
+    return result;
   }
 
+  /// use a [GlobalKey] to generate structure
   Map<String, dynamic> get structure => _dumpToJson();
+
+  /// load neural networks structure
+  void _fromJson() {
+    /// TODO
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.root != null) {
+      graph = Graph(
+          nodeMessageMap: widget.nodeMessageMap,
+          messageStyle: widget.messageStyle,
+          nodes: _linearNodes(context, widget.root!),
+          direction: widget.direction,
+          centerLayout: true,
+          onEdgeColor: widget.onEdgeColor);
+    } else {
+      _fromJson();
+      graph = Graph(
+          nodeMessageMap: widget.nodeMessageMap,
+          messageStyle: widget.messageStyle,
+          nodes: _linearNodes(context, widget.root!),
+          direction: widget.direction,
+          centerLayout: true,
+          onEdgeColor: widget.onEdgeColor);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +87,10 @@ class NNFlowGraphView<T extends NNNode> extends StatelessWidget {
         manager: _focusManager,
         child: Builder(
           builder: (context) {
-            graph = Graph(
-                nodeMessageMap: nodeMessageMap,
-                messageStyle: messageStyle,
-                nodes: _linearNodes(context, root),
-                direction: direction,
-                centerLayout: true,
-                onEdgeColor: onEdgeColor);
-
             return GestureDetector(
               onTap: () {
                 GraphFocus.of(context).clearFocus();
-                onSelectChanged?.call(null);
+                widget.onSelectChanged?.call(null);
               },
               child: GraphView<T>(graph: graph),
             );
