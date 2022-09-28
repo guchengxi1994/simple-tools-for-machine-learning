29ceb1246272356e7a713243803a18bd8c07e4e6
壹零 <guchengxi1994@qq.com>
2022-09-28 15:58:16 +0800

    fix bug

--- mltools/src/augmentation/labelme/get_region.py
+++ mltools/src/augmentation/labelme/get_region.py
@@ -1,28 +1,31 @@
+import cv2
 import numpy as np
-from skimage.measure import find_contours, approximate_polygon
 
-# from skimage.draw import polygon
-# from skimage import io
-from mltools.src.log.logger import logger
+
+def get_approx(contour, length_p=0.1):
+    """获取逼近多边形
+    :param img: 处理图片
+    :param contour: 连通域
+    :param length_p: 逼近长度百分比
+    """
+    # 逼近长度计算
+    epsilon = length_p * cv2.arcLength(contour, True)
+    # 获取逼近多边形
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    return approx
 
 
 def get_multi_region(img_bin):
-    # _img = np.zeros(img_bin.shape)
-    contours = find_contours(img_bin)
-    # print(len(contours))
+    contours, _ = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     regions = []
-    if len(contours) > 0:
-        for i in contours:
-            coords = approximate_polygon(i, tolerance=2.5)
-            # print(coords.shape[0])
-            if coords.shape[0] > 3:
-                regions.append(coords)
-                # rr, cc = polygon(coords[:, 0], coords[:, 1], img_bin.shape)
-                # _img[rr, cc] = 255
-
-        # io.imsave("dddd.jpg", _img)
-    else:
-        # logger.info("no contours find")
-        pass
+    if len(contours) >= 1:
+        for i in range(0, len(contours)):
+            if contours[i] != []:
+                region = get_approx(contours[i], 0.002)
+                if region.shape[0] > 3:
+                    _r = []
+                    for j in region:
+                        _r.append(j[0])
+                    regions.append(np.array(_r))
 
     return regions
